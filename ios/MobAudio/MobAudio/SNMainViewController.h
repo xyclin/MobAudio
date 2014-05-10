668a9bb49038ef9844f71d551192696a4a4ca4f1
Shahmeer Navid <shahmeernavid@gmail.com>
2014-05-10 13:23:15 -0700

    root view ios

--- ios/MobAudio/MobAudio/SNMainViewController.h
+++ ios/MobAudio/MobAudio/SNMainViewController.h
@@ -1,5 +1,5 @@
 //
-//  SNViewController.h
+//  SNMainViewController.h
 //  MobAudio
 //
 //  Created by Shahmeer Navid on 5/10/14.
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SNViewController : UIViewController
+@interface SNMainViewController : UITableViewController
+
+
 
 @end
