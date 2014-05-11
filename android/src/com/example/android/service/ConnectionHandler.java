3a76942c735d04ba862c24fa1bdf29883a86c1dc
andyzg <zhangandy94@gmail.com>
2014-05-10 19:31:34 -0700

    Added socket implementation to connct to web server

--- android/src/com/example/android/service/ConnectionHandler.java
+++ android/src/com/example/android/service/ConnectionHandler.java
@@ -1,7 +1,7 @@
 package com.example.android.service;
 
 public interface ConnectionHandler {
-    public void connect(String url);
+    public void connect();
     public boolean isConnected();
     // TODO: Methods for interacting with web server
 }
--- android/src/com/example/android/service/HostManager.java
+++ android/src/com/example/android/service/HostManager.java
@@ -1,13 +1,71 @@
 package com.example.android.service;
 
+import android.content.Context;
+import android.widget.Toast;
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+
 public class HostManager implements ConnectionHandler {
     private boolean isConnected;
-    public HostManager() {
+    private Context context;
+    public HostManager(Context context) {
         this.isConnected = false;
+        this.context = context;
     }
 
-    public void connect(String address) {
+    public void connect() {
+        SocketIO socket;
+        try {
+            socket = new SocketIO("192.241.208.189:54321");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return;
+        }
 
+        socket.connect(new IOCallback() {
+
+            @Override
+            public void onDisconnect() {
+
+            }
+
+            @Override
+            public void onConnect() {
+                isConnected = true;
+            }
+
+            @Override
+            public void onMessage(String s, IOAcknowledge ioAcknowledge) {
+
+            }
+
+            @Override
+            public void onMessage(JSONObject jsonObject, IOAcknowledge ioAcknowledge) {
+
+            }
+
+            @Override
+            public void on(String s, IOAcknowledge ioAcknowledge, Object... objects) {
+                if (s == "heartbeat") {
+                    handleHeartbeat(objects);
+                }
+            }
+
+            @Override
+            public void onError(SocketIOException e) {
+
+            }
+        });
+    }
+
+    public void handleHeartbeat(Object objects) {
+        Toast.makeText(context, "Sup",
+                Toast.LENGTH_SHORT).show();
     }
 
     public boolean isConnected() {
