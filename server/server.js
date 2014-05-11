d3b2d3b4e87259d325831bb60de7401aece7c921
Ted Ying <yingted@gmail.com>
2014-05-10 22:34:01 -0700

    rest

--- server/server.js
+++ server/server.js
@@ -34,11 +34,99 @@ app.post('/upload', function(req, res) {
 	});
 });
 
+app.get('/youtube', function(req, res) {
+	var rand = uuid.v4();
+	exec('youtube-dl -x --audio-format mp3 -q -o \''+rand+'.%(ext)s\' "$url"', {
+		env: { url: req.params.url },
+		cwd: process.cwd+'/static/',
+	}, function(err, stdout, stderr) {
+		if(err) {
+			res.send(500);
+		} else {
+			res.send('http://'+req.host+':54322/'+rand+'.mp3');
+		}
+	});
+});
+
 var _nextClientId = 0;
 var _nextMobId = 0;
 var _subscribers = {};
 var _data = {};
 var _clients = {};
+
+var api = {
+	list: function(data) {
+		return {
+			id: data.id,
+			mobs: Object.keys(_data).map(function(mobId) {
+				var mob = _data[mobId];
+				var r = 6378100, RATIO = Math.PI/180;
+				mob.dist = 2*r*Math.asin(Math.sqrt(Math.pow(Math.sin((mob.lat-data.lat)/2*RATIO), 2)+Math.cos(data.lat*RATIO)*Math.cos(mob.lat*RATIO)*Math.pow((mob.lon-data.lon)/2*RATIO, 2)));
+				return mob;
+			}).filter(function(mob) {
+				return !mob.done && mob.dist <= data.radius;
+			}).sort(function(a, b) {
+				return a.dist-b.dist;
+			}),
+		};
+	},
+	subscribe: function(data) {
+		console.assert(typeof data.mobId == 'number');
+		_subscribers[data.mobId][data._clientId] = true;
+		if(_data[data.mobId].done) {
+			data.mobId = -1;
+		}
+		++_data[data.mobId].count;
+		broadcast('count', data);
+	},
+	unsubscribe: function(data) {
+		console.assert(typeof data.mobId == 'number');
+		delete _subscribers[data.mobId][_clientId];
+		--_data[data.mobId].count;
+		broadcast('count', data);
+	},
+	create: function(data) {
+		var mobId = _nextMobId++;
+		_subscribers[mobId] = {};
+		_subscribers[mobId][_clientId] = true;
+		_data[mobId] = data;
+		data.count = 1;
+		data.mobId = mobId;
+		data._clientId = _clientId;
+	},
+	play: function(data) {
+		console.assert(_clientId == data._clientId);
+		_data[data.mobId].done = true;
+		broadcast('play', {
+			id: data.id,
+			mobId: data.mobId,
+		});
+		return null;
+	},
+	destroy: function(data) {
+		var subs = _subscribers[data.mobId];
+		delete _subscribers[data.mobId];
+		broadcast('unsubscribe', {
+			id: -1,
+			mobId: data.mobId,
+		});
+		delete _data[data.mobId];
+	},
+};
+
+Object.keys(api).map(function(func) {
+	app.post('/'+func, function(req, res) {
+		res.send(JSON.stringify(api[func](req.body)));
+	});
+});
+
+function broadcast(evt, data) {
+	var subs = _subscribers[data.mobId];
+	for(var cid in subs)
+		if(subs.hasOwnProperty(cid))
+			_clients[cid].emit(evt, data, handler);
+}
+
 io.sockets.on('connection', function(socket) {
 	var _clientId = _nextClientId++;
 	_clients[_clientId] = socket;
@@ -51,85 +139,12 @@ io.sockets.on('connection', function(socket) {
 		socket.emit('heartbeat', { timestamp: Date.now() }, handler);
 		setTimeout(heartbeat, 5000);
 	})();
-	function broadcast(evt, data) {
-		var subs = _subscribers[data.mobId];
-		for(var cid in subs)
-			if(subs.hasOwnProperty(cid))
-				_clients[cid].emit(evt, data, handler);
-	}
-	socket.on('list', function(data) {
-		socket.emit('list', {
-			id: data.id,
-			mobs: Object.keys(_data).map(function(mobId) {
-				var mob = _data[mobId];
-				var r = 6378100, RATIO = Math.PI/180;
-				mob.dist = 2*r*Math.asin(Math.sqrt(Math.pow(Math.sin((mob.lat-data.lat)/2*RATIO), 2)+Math.cos(data.lat*RATIO)*Math.cos(mob.lat*RATIO)*Math.pow((mob.lon-data.lon)/2*RATIO, 2)));
-				return mob;
-			}).filter(function(mob) {
-				return !mob.done && mob.dist <= data.radius;
-			}).sort(function(a, b) {
-				return a.dist-b.dist;
-			}),
-		}, handler);
+	Object.keys(api).map(function(func) {
+		socket.on(func, function(data) {
+			data._clientId = _clientId;
+			var msg = api[func](data);
+			if (msg !== null)
+				socket.emit(func, msg === undefined ? data : msg, handler);
+		}.protectWith(handler));
 	});
-	socket.on('subscribe', function(data) {
-		console.assert(typeof data.mobId == 'number');
-		_subscribers[data.mobId][_clientId] = true;
-		if(_data[data.mobId].done) {
-			data.mobId = -1;
-		}
-		++_data[data.mobId].count;
-		broadcast('count', data);
-		socket.emit('subscribe', data, handler);
-	}.protectWith(handler));
-	socket.on('unsubscribe', function(data) {
-		console.assert(typeof data.mobId == 'number');
-		delete _subscribers[data.mobId][_clientId];
-		--_data[data.mobId].count;
-		broadcast('count', data);
-		socket.emit('unsubscribe', data, handler);
-	}.protectWith(handler));
-	socket.on('create', function(data) {
-		var mobId = _nextMobId++;
-		_subscribers[mobId] = {};
-		_subscribers[mobId][_clientId] = true;
-		_data[mobId] = data;
-		data.count = 1;
-		data.mobId = mobId;
-		data._clientId = _clientId;
-		socket.emit('create', data, handler);
-	}.protectWith(handler));
-	socket.on('destroy', function(data) {
-		var subs = _subscribers[data.mobId];
-		delete _subscribers[data.mobId];
-		broadcast('unsubscribe', {
-			id: -1,
-			mobId: data.mobId,
-		});
-		delete _data[data.mobId];
-		socket.emit('destroy', data, handler);
-	}.protectWith(handler));
-	socket.on('play', function(data) {
-		console.assert(_clientId == data._clientId);
-		_data[data.mobId].done = true;
-		broadcast('play', {
-			id: data.id,
-			mobId: data.mobId,
-		});
-	}.protectWith(handler));
-	socket.on('youtube', function(data) {
-		var rand = uuid.v4();
-		exec('youtube-dl -x --audio-format mp3 -q -o \''+rand+'.%(ext)s\' "$url"', {
-			url: data.url,
-			cwd: process.cwd+'/static/',
-		}, function(err, stdout, stderr) {
-			if(err) {
-				data.err = err;
-				data.upload = null;
-			} else {
-				data.upload = 'http://'+req.host+':54322/'+rand+'.mp3';
-			}
-			socket.emit('youtube', data, handler);
-		});
-	}.protectWith(handler));
 });
